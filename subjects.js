5286aa5f2f878851a83e6a0432bc1db04ef435d8
alakhnamdev <namdev.alakh@outlook.com>
2024-11-28 02:00:45 +0530

    moved file

--- subjects.js
+++
@@ -1,1 +0,0 @@
-let subjects = ['English', 'Hindi', 'Sanskrit', 'Mathematics', 'Environmental Studies', 'Art & Craft', 'Physical Education', 'Computer Education', 'Science', 'Physics', 'Chemistry', 'Biology', 'Social Science', 'History', 'Geography', 'Political Science', 'Economics', 'Information Technology', 'Computer Science', 'Accountancy', 'Business Studies', 'Psychology'];
